28cbcff40818309a339689dc71d76b7e4c5aa743
tanphandev <phantanbn31@gmail.com>
2023-05-18 20:17:36 +0700

    chore: hide actionbar Register UI and clean code

--- Plant/app/src/main/java/com/example/plant/activities/ForgotPassword/ForgotAccountActivity.kt
+++ Plant/app/src/main/java/com/example/plant/activities/ForgotPassword/ForgotAccountActivity.kt
@@ -11,47 +11,46 @@ import com.google.firebase.auth.FirebaseAuth
 import android.util.Patterns
 import com.example.plant.R
 import com.example.plant.activities.Login.LoginActivity
+import kotlinx.android.synthetic.main.activity_forgot_account.btnReset
+import kotlinx.android.synthetic.main.activity_forgot_account.etForgetEmail
 
 class ForgotAccountActivity : AppCompatActivity() {
-    private lateinit var emailEdit: EditText
-    private lateinit var btnReset: Button
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_account)
+
+        supportActionBar?.hide()
+
         mAuth = FirebaseAuth.getInstance()
-
-        emailEdit = findViewById(R.id.etForgetEmail)
-
-        btnReset = findViewById(R.id.btnReset)
 
         btnReset.setOnClickListener() {
             reset()
         }
     }
     private fun reset() {
-        val emailStr = emailEdit.text.toString().trim()
+        val emailStr = etForgetEmail.text.toString().trim()
         if(TextUtils.isEmpty(emailStr)){
-            Toast.makeText(this, "Nhập địa chỉ email!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Enter your Email!", Toast.LENGTH_SHORT).show()
             return
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(emailStr).matches()) {
-            emailEdit.error = "Email sai định dạng"
-            emailEdit.requestFocus()
+            etForgetEmail.error = "Please enter a valid email address!"
+            etForgetEmail.requestFocus()
             return
         }
         mAuth.sendPasswordResetEmail(emailStr)
             .addOnCompleteListener(this){task->
                 if (task.isSuccessful()){
-                    Toast.makeText(this, "Kiểm tra email và reset mật khẩu!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Please check your email and change password!", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, LoginActivity::class.java)
                     startActivity(intent)
                 }
                 else
                 {
-                    Toast.makeText(this, "Thất bại!", Toast.LENGTH_SHORT).show()
-                    emailEdit.requestFocus()
+                    Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                    etForgetEmail.requestFocus()
                 }
 
             }
