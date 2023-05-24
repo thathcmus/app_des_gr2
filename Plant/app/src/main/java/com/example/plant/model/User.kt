1bd67be92f3b302d6110362bb76802f23d3f41bc
tanphandev <phantanbn31@gmail.com>
2023-05-24 08:54:34 +0700

    chore: Change datatype of Phone field

--- Plant/app/src/main/java/com/example/plant/model/User.kt
+++ Plant/app/src/main/java/com/example/plant/model/User.kt
@@ -1,4 +1,4 @@
 package com.example.plant.model
 
-class User(var id: String = "",  var fullName: String = "", var email : String = "", var phone: String = "", var gender : String = "", var location: String = "",
+class User(var id: String = "",  var fullName: String = "", var email : String = "", var phone: Number = 0, var gender : String = "", var location: String = "",
            var avatar : String = "")
