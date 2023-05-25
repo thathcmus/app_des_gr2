4bdc5a493ee6484470d5f452c40ff565474f9a2d
tanphandev <phantanbn31@gmail.com>
2023-05-25 08:57:00 +0700

    refactor: refactor code, sperate firestore and activity

--- Plant/app/src/main/java/com/example/plant/activities/Profile/ProfileActivity.kt
+++ Plant/app/src/main/java/com/example/plant/activities/Profile/ProfileActivity.kt
@@ -3,54 +3,36 @@ package com.example.plant.activities.Profile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.core.view.isVisible
 import com.example.plant.R
 import com.example.plant.activities.UpdateProfile.UpdateProfileActivity
 import com.example.plant.constant.constant
+import com.example.plant.firestore.Firestore
 import com.example.plant.model.User
-import com.example.plant.util.ProgressBarLoading
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_profile.editUser
 import kotlinx.android.synthetic.main.activity_profile.ivLocationIcon
 import kotlinx.android.synthetic.main.activity_profile.tvLocationUser
 import kotlinx.android.synthetic.main.activity_profile.tvNameUser
 
 class ProfileActivity : AppCompatActivity() {
-    private val currentUserAuth = Firebase.auth.currentUser
-    private val db = Firebase.firestore
     private var currentUser = User()
-    private val loadingDialog = ProgressBarLoading(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         supportActionBar?.hide()
-        getUserDetail()
+        Firestore().getUserDetail(this)
         listenerEvent()
 
     }
-    fun getUserDetail() {
-        loadingDialog.startLoading()
-        db.collection("users").document(currentUserAuth!!.uid)
-        .get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    currentUser = document.toObject(User::class.java)!!
-                    tvNameUser.text = currentUser?.fullName
-                    tvLocationUser.text = currentUser?.location
-                    if (currentUser?.location == "") {
-                        ivLocationIcon.isVisible = false
-                    }
-                    loadingDialog.hideLoading()
-                } else {
-                    Log.e("get database", "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.e("get database", "get userDetail failed", exception)
-            }
+    fun setCurrentUser(currentUser: User){
+        this.currentUser = currentUser
+    }
+    fun ShowUIInfo(UserInfo : User){
+        tvNameUser.text = UserInfo?.fullName
+        tvLocationUser.text = UserInfo?.location
+        if (UserInfo?.location == "") {
+            ivLocationIcon.isVisible = false
+        }
     }
     fun listenerEvent(){
         editUser.setOnClickListener(){
--- Plant/app/src/main/java/com/example/plant/firestore/Firestore.kt
+++ Plant/app/src/main/java/com/example/plant/firestore/Firestore.kt
@@ -1,15 +1,24 @@
 package com.example.plant.firestore
 
+import android.app.Activity
+import android.util.Log
 import android.widget.Toast
+import androidx.core.view.isVisible
+import com.example.plant.activities.Profile.ProfileActivity
 import com.example.plant.activities.RegisterAccount.RegisterActivity
 import com.example.plant.constant.constant
 import com.example.plant.model.User
+import com.example.plant.util.ProgressBarLoading
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_profile.ivLocationIcon
+import kotlinx.android.synthetic.main.activity_profile.tvLocationUser
+import kotlinx.android.synthetic.main.activity_profile.tvNameUser
 
 class Firestore {
     private val mFireStore = Firebase.firestore
-
     fun registerUser(mActivity: RegisterActivity, UserInfo: User)
     {
         mFireStore.collection(constant.USER_COLLECTION).document(UserInfo.id)
@@ -19,6 +28,33 @@ class Firestore {
             }
             .addOnFailureListener {
                 Toast.makeText(mActivity, "Store User Information failed!!!", Toast.LENGTH_SHORT).show()
+            }
+    }
+    fun getCurrentUserAuth(): FirebaseUser? {
+        return Firebase.auth.currentUser
+    }
+
+    fun getUserDetail(mActivity: Activity) {
+        val loadingDialog = ProgressBarLoading(mActivity)
+        loadingDialog.startLoading()
+        mFireStore.collection("users").document(getCurrentUserAuth()!!.uid)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val currentUser = document.toObject(User::class.java)!!
+                    when(mActivity) {
+                        is ProfileActivity -> {
+                            mActivity.setCurrentUser(currentUser)
+                            mActivity.ShowUIInfo(currentUser)
+                        }
+                    }
+                    loadingDialog.hideLoading()
+                } else {
+                    Log.e("get database", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("get database", "get userDetail failed", exception)
             }
     }
 
