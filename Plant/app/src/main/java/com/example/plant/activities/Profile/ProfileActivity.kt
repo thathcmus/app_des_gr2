9f3dcf00e373dabb589b7ea93a0459d18b68b829
Httigh <htthinh0@gmail.com>
2023-06-02 20:21:04 +0700

    create 'home fragment', 'adding fragment', 'profile activity'

---
+++ Plant/app/src/main/java/com/example/plant/activities/Profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plant.activities.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plant.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
