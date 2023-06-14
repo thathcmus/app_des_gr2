8dc48bfa7094e37dc148e0246b54e44b46aaf5ef
Httigh <htthinh0@gmail.com>
2023-06-14 21:06:21 +0700

    Edit: After adding a new plant, return to the home screen. Add click event for floating button to switch to AddingNewActivity. Add photography data input validation error message.

--- Plant/app/src/main/java/com/example/plant/fragment/PhotographyInputDataFragment.kt
+++ Plant/app/src/main/java/com/example/plant/fragment/PhotographyInputDataFragment.kt
@@ -32,14 +32,132 @@ class PhotographyInputDataFragment(private val imageUri: Uri?) : BottomSheetDial
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnSave.setOnClickListener {
-            uploadImage()
+            if(isValidateInput()) {
+                uploadImage()
+            }
         }
 
         storageRef = FirebaseStorage.getInstance().reference.child("Photography")
         firebaseFirestore = FirebaseFirestore.getInstance()
 
+    }
 
+    companion object{
+        const val ERROR_PLANT_NAME_EMPTY = "The plant name must not be empty"
+        const val ERROR_PLANT_NAME_LENGTH = "The plant name cannot exceed 30 characters"
+        const val ERROR_PLANT_DESCRIPTION_EMPTY = "The plant description must not be empty"
+        const val ERROR_PLANT_DESCRIPTION_LENGTH = "This field requires a minimum of 50 characters"
+        const val ERROR_PLANT_FAMILY_EMPTY = "The plant family must not be empty"
+        const val ERROR_PLANT_KINGDOM_EMPTY = "The plant kingdom must not be empty"
+        const val ERROR_PLANT_TYPE_EMPTY = "The type of plant must not be empty"
+        const val ERROR_PLANT_STAR_EMPTY = "The plant star must not be empty"
+        const val ERROR_PLANT_SPECIES_RANGE = "This field only accepts a number from 0 to 5"
+    }
 
+    private fun validatePlantName(plantName: String): Pair<Boolean, String> {
+        if (plantName.isEmpty()) {
+            return Pair(false, ERROR_PLANT_NAME_EMPTY)
+        } else if (plantName.length > 30) {
+            return Pair(false, ERROR_PLANT_NAME_LENGTH)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantDescription(plantDescription: String): Pair<Boolean, String> {
+        if (plantDescription.isEmpty()) {
+            return Pair(false, ERROR_PLANT_DESCRIPTION_EMPTY)
+        } else if (plantDescription.length < 50) {
+            return Pair(false, ERROR_PLANT_DESCRIPTION_LENGTH)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantFamily(plantFamily: String): Pair<Boolean, String> {
+        if (plantFamily.isEmpty()) {
+            return Pair(false, ERROR_PLANT_FAMILY_EMPTY)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantKingdom(plantKingdom: String): Pair<Boolean, String> {
+        if (plantKingdom.isEmpty()) {
+            return Pair(false, ERROR_PLANT_KINGDOM_EMPTY)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantType(plantType: String): Pair<Boolean, String> {
+        if (plantType.isEmpty()) {
+            return Pair(false, ERROR_PLANT_TYPE_EMPTY)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantStar(plantStar: String): Pair<Boolean, String> {
+        if (plantStar.isEmpty()) {
+            return Pair(false, ERROR_PLANT_STAR_EMPTY)
+        }
+        return Pair(true, "")
+    }
+
+    private fun validatePlantSpecies(PlantSpecies: String): Pair<Boolean, String> {
+        if (PlantSpecies.isEmpty()) {
+            return Pair(false, ERROR_PLANT_SPECIES_RANGE)
+        }
+        return Pair(true, "")
+    }
+
+    private fun isValidateInput(): Boolean {
+        val plantName = binding.photoName.text?.trim().toString()
+        val (isValidatePlantName, PlantNameMessError) = validatePlantName(plantName)
+        if (!isValidatePlantName) {
+            binding.photoName.error = PlantNameMessError
+        }
+
+        val plantDescription = binding.photoDescription.text?.trim().toString()
+        val (isValidatePlantDescription, PlantDescriptionMessError) = validatePlantDescription(plantDescription)
+        if (!isValidatePlantDescription) {
+            binding.photoDescription.error = PlantDescriptionMessError
+        }
+
+        val plantFamily = binding.photoFamily.text?.trim().toString()
+        val (isValidatePlantFamily, PlantFamilyMessError) = validatePlantFamily(plantFamily)
+        if (!isValidatePlantFamily) {
+            binding.photoFamily.error = PlantFamilyMessError
+        }
+
+        val plantKingdom = binding.photoKingdom.text?.trim().toString()
+        val (isValidatePlantKingdom, PlantKingdomMessError) = validatePlantKingdom(plantKingdom)
+        if (!isValidatePlantKingdom) {
+            binding.photoKingdom.error = PlantKingdomMessError
+        }
+
+        val plantType = binding.photoPlantType.text?.trim().toString()
+        val (isValidatePlantType, PlantTypeMessError) = validatePlantType(plantType)
+        if (!isValidatePlantType) {
+            binding.photoPlantType.error = PlantTypeMessError
+        }
+
+        val plantStar = binding.photoStar.text?.trim().toString()
+        val (isValidatePlantStar, PlantStarMessError) = validatePlantStar(plantStar)
+        if (!isValidatePlantStar) {
+            binding.photoStar.error = PlantStarMessError
+        }
+
+        val plantSpecies = binding.photoSpecies.text?.trim().toString()
+        val (isValidatePlantSpecies, PlantSpeciesMessError) = validatePlantSpecies(plantSpecies)
+        if (!isValidatePlantSpecies) {
+            binding.photoSpecies.error = PlantSpeciesMessError
+        }
+
+        if(isValidatePlantName && isValidatePlantDescription
+            && isValidatePlantFamily && isValidatePlantKingdom
+            && isValidatePlantType && isValidatePlantStar
+            && isValidatePlantSpecies){
+            return true
+        }
+
+        return false
     }
 
     private fun uploadImage() {
