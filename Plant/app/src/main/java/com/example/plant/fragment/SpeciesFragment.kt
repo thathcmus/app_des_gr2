7ca8a830ca50fd8e1abc021618333eef81d6d8e4
Httigh <htthinh0@gmail.com>
2023-06-26 14:17:29 +0700

    add Search bar for Species layout

--- Plant/app/src/main/java/com/example/plant/fragment/SpeciesFragment.kt
+++ Plant/app/src/main/java/com/example/plant/fragment/SpeciesFragment.kt
@@ -4,22 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.plant.R
 import com.example.plant.adapter.SpeciesRecyclerAdapter
 import com.example.plant.constant.constant
 import com.example.plant.databinding.FragmentSpeciesBinding
+import com.example.plant.databinding.FragmentSpeciesTestForSearchBinding
 import com.example.plant.model.Species
 import com.example.plant.util.FragmentUtil
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
+
 class SpeciesFragment :  Fragment(), SpeciesRecyclerAdapter.MyClickListener {
 
     private lateinit var binding: FragmentSpeciesBinding
     private var speciesList: MutableList<Species> = mutableListOf()
+
+    // biến này copy speciesList, dùng trong event của searchView
+    private var speciesListOrigin: MutableList<Species> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +44,7 @@ class SpeciesFragment :  Fragment(), SpeciesRecyclerAdapter.MyClickListener {
             .get()
             .addOnSuccessListener { species ->
                 speciesList  = species.toObjects(Species::class.java)
+                speciesListOrigin = speciesList
                 //show into recycleview
                 binding.rcSpecies.adapter = this.activity?.let {
                     SpeciesRecyclerAdapter( speciesList , this@SpeciesFragment)
@@ -78,4 +84,43 @@ class SpeciesFragment :  Fragment(), SpeciesRecyclerAdapter.MyClickListener {
         FragmentUtil(this.activity).replaceFragment(PlantFragment,
             R.id.HomeFrameLayout,true)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // androidx Search view to species
+        binding.searchSpecies.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+
+                // speciesList nhận lại danh sách dữ liệu đã lấy ban đầu trên firestore
+                speciesList = speciesListOrigin
+
+                // Tạo filteredSpeciesList dựa trên speciesList và input vào search
+                val filteredSpeciesList = speciesList.filter { species ->
+                    species.name.contains(newText.trim(), ignoreCase = true)
+                }
+
+                // đưa adapter filteredSpeciesList vào rcSpecies để hiển thị danh sách tìm kiếm
+                binding.rcSpecies.adapter =
+                    SpeciesRecyclerAdapter(filteredSpeciesList as MutableList<Species>,
+                    this@SpeciesFragment)
+
+                // gán filteredSpeciesList vào speciesList, việc này nhằm
+                // khiến cho hành động click vào items sẽ là click vào filteredSpeciesList
+                // speciesList sẽ được gán lại dữ liệu ban đầu sau mỗi lần thay đổi nội dung nhập
+                // trên search
+                speciesList = filteredSpeciesList
+
+
+                return false
+            }
+
+        })
+    }
+
 }
